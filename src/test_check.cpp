15ccd738c67f6980cd675806d15bd38babae2761
mark <hzypp@sina.cn>
2021-07-29 21:28:58 +0800

    fix test check

--- src/test_check.cpp
+++ src/test_check.cpp
@@ -5,35 +5,35 @@ struct A{};
 struct B{ float f; };
 struct C{ void f(){} };
 struct D{ void f(int){} };
-struct E{ int f(){} };
+struct E{ int f(){return 0;} };
 struct F{ private:float f; };
 
 namespace {
-    template<typename T,typename=int T::*>        std::true_type  test1();
+    template<typename T,typename=int T::*>        std::true_type  test1(int);
     template<typename T>                          std::false_type test1(...);
-    template<typename T,typename=decltype(&T::f)> std::true_type  test2();
+    template<typename T,typename=decltype(&T::f)> std::true_type  test2(int);
     template<typename T>                          std::false_type test2(...);
-    template<typename T,typename=decltype(T::f)>  std::true_type  test3();
+    template<typename T,typename=decltype(T::f)>  std::true_type  test3(int);
     template<typename T>                          std::false_type test3(...);
     template<typename T,typename=decltype(std::declval<T>().f())>     
-                                                  std::true_type  test4();
+                                                  std::true_type  test4(int);
     template<typename T>                          std::false_type test4(...);
     template<typename T,typename U=std::decay_t<decltype(std::declval<T>().f())>
     ,typename=std::enable_if_t<std::is_same_v<int,U>>> 
-                                                  std::true_type  test5();
+                                                  std::true_type  test5(int);
     template<typename T>                          std::false_type test5(...);
 }
 
 template<typename T>
-inline constexpr bool check1_v = decltype(::test1<T>())::value;
+inline constexpr bool check1_v = decltype(::test1<T>(0))::value;
 template<typename T>
-inline constexpr bool check2_v = decltype(::test2<T>())::value;
+inline constexpr bool check2_v = decltype(::test2<T>(0))::value;
 template<typename T>
-inline constexpr bool check3_v = decltype(::test3<T>())::value;
+inline constexpr bool check3_v = decltype(::test3<T>(0))::value;
 template<typename T>
-inline constexpr bool check4_v = decltype(::test4<T>())::value;
+inline constexpr bool check4_v = decltype(::test4<T>(0))::value;
 template<typename T>
-inline constexpr bool check5_v = decltype(::test5<T>())::value;
+inline constexpr bool check5_v = decltype(::test5<T>(0))::value;
 
 template<typename T>
 void check_f(const char name[]){
